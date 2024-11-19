14b9138c7f696cc9c68abed46d6dba4e207a7a4d
garritr01 <garritr01@gmail.com>
2024-11-18 20:46:50 -0500

    Add most recent selectors and log authority

--- src/ROSA/src/oddsAndEnds.js
+++ src/ROSA/src/oddsAndEnds.js
@@ -63,7 +63,6 @@ export function getWeekdayString(weekdayNum) {
  * the object converted from UTC to local
  */
 export function convertUTCstringsToLocal(dateTime) {
-
     const utcDate = new Date(dateTime.date + ' ' + dateTime.time);
     const localDate = new Date(utcDate.getTime() - (utcDate.getTimezoneOffset() * 60000));
     return formatDateToObject(localDate);
@@ -173,3 +172,108 @@ export const chooseMostRecent = (files, directory, title) => {
     }
 }
 
+/** 
+ * Get the most recent version of a file from a list of file info.
+ * 
+ * @param {Object[]} fileInfo - An array of file objects containing file details.
+ * @param {string} fileInfo[].directory - The directory path of the file.
+ * @param {string} fileInfo[].filename - The name of the file.
+ * @param {Object} fileInfo[].dateTime - The timestamp of the file.
+ * @param {string} fileInfo[].dateTime.date - The date of the file version in 'MM-DD-YYYY' format.
+ * @param {string} fileInfo[].dateTime.time - The time of the file version in 'HH:MM' format.
+ * 
+ * @param {string} dir - The directory to filter the files by.
+ * @param {string} filename - The filename to filter the files by.
+ * 
+ * @returns - {date: '', time: ''} filled or empty if not found
+ */
+export const newChooseMostRecent = (fileInfo, dir, filename) => {
+    let count = 0;
+    let dates = [];
+    //console.log(files,directory,title);
+    for (const possibility of fileInfo) {
+        if (possibility.directory === dir && possibility.filename === filename) {
+            dates.push(possibility);
+            count++;
+        }
+    }
+    if (dates.length > 0) {
+        let mostRecentDateObject = new Date(dates[0].dateTime.date + ' ' + dates[0].dateTime.time);
+        let mostRecentDateTime = dates[0].dateTime;
+        if (dates.length > 1) {
+            let testDateObject;
+            for (let i = 1; i < count; i++) {
+                testDateObject = new Date(dates[i].dateTime.date + ' ' + dates[i].dateTime.time);
+                if (testDateObject > mostRecentDateObject) {
+                    mostRecentDateObject = testDateObject;
+                    mostRecentDateTime = dates[i].dateTime;
+                }
+            }
+        }
+        return mostRecentDateTime;
+    } else {
+        return { date: '', time: ''};
+    }
+}
+
+/** 
+ * Get the most recent version of a file from a list of file info.
+ * 
+ * @param {Object[]} versions - An array of versions
+ * @param {string} versions[].date - The date of the file version in 'MM-DD-YYYY' format.
+ * @param {string} versions[].time - The time of the file version in 'HH:MM' format.
+ * 
+ * @returns - {date: '', time: ''} filled or empty if not found
+ */
+export const newChooseMostRecentSimple = (versions) => {
+
+    if (versions.length > 0) {
+        let mostRecentDateObject = new Date(versions[0].date + ' ' + versions[0].time);
+        let mostRecentDateTime = versions[0];
+        versions.forEach((version) => {
+            let testDateObject = new Date(version.date + ' ' + version.time);
+            if (testDateObject > mostRecentDateObject) {
+                mostRecentDateObject = testDateObject;
+                mostRecentDateTime = version;
+            }
+        });
+        return mostRecentDateTime;
+    } else {
+        return { date: '', time: '' };
+    }
+}
+
+/**
+ * Determines whether to log and how verbose the logging should be based on the provided log level and characters.
+ *
+ * @param {string[]} logLevel - An array of strings representing the current logging levels.
+ * @param {string[]} characters - The characters to check for in the logging levels.
+ * @returns {number} - Returns 2 if the log level includes one of the characters followed by 'v',
+ *                     1 if the log level includes just one of the characters,
+ *                     and 0 otherwise.
+ * 
+ * @example 
+ * logCheck(['e','bv','o'],['b','e']) = 2
+ * 
+ * @example
+ * logCheck(['ev','bv','o'],['o','d']) = 1
+ */
+export const logCheck = (logLevel, characters) => {
+    
+    // return 2 if char+'v' exists
+    for (const character of characters) {
+        if (logLevel.includes(character+'v')) {
+            return 2;
+        }
+    }
+
+    // return 1 if char+'v' DNE and char exists
+    for (const character of characters) {
+        if (logLevel.includes(character)) {
+            return 1;
+        }
+    }
+        
+    // return 0 if char and char+'v' DNE
+    return 0;
+}
